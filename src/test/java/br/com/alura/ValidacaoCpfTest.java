9be9272209c3461030d3afa4b8d045a9b7695a50
wellington.siqueira <wellingtonex@gmail.com>
2017-01-26 15:42:36 -0300

    adicionando e removendo formatacao do cpf

--- src/test/java/br/com/alura/ValidacaoCpfTest.java
+++ src/test/java/br/com/alura/ValidacaoCpfTest.java
@@ -1,7 +1,10 @@
 package br.com.alura;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+import br.com.caelum.stella.format.CPFFormatter;
 import br.com.caelum.stella.validation.CPFValidator;
 import br.com.caelum.stella.validation.InvalidStateException;
 
@@ -19,5 +22,19 @@ public class ValidacaoCpfTest {
 		String cpf = "99988399120";
 		CPFValidator validator = new CPFValidator();
 		validator.assertValid(cpf);
+	}
+	
+	@Test
+	public void removeFormatacaoTest() {
+		String cpf = "862.883.667-57";
+		CPFFormatter formatter = new CPFFormatter();
+		assertEquals("86288366757", formatter.unformat(cpf));
+	}
+	
+	@Test
+	public void formataTest() {
+		String cpf = "86288366757";
+		CPFFormatter formatter = new CPFFormatter();
+		assertEquals("862.883.667-57", formatter.format(cpf));
 	}
 }
